b2a7c43fe9eed714d66a15884f9af9887c29e73d
Matias Rizzi <rizzijmatias@gmail.com>
2023-06-26 18:12:43 -0300

    add view controller

--- app/loan/views.py
+++ app/loan/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse, HttpResponseNotAllowed
+import requests
+from requests.auth import HTTPBasicAuth
 
-# Create your views here.
+from loan.models import Loan
+
+STATE_APPROVE = 'approve'
+
+def index(request):
+    return HttpResponse("Test")
+
+def controller(request):
+    '''
+    intermediate endpoint. Response json with aprove or disallow loan
+    '''
+    auth = HTTPBasicAuth('user', 'ZGpzOTAzaWZuc2Zpb25kZnNubm5u')
+    #TODO manage variable dni and add it the end to url
+    dni = 30156149
+
+    url = f'https://api.moni.com.ar/api/v4/scoring/pre-score/{dni}'
+    result = requests.get(url, auth=auth)
+
+    response = {'state':result.json()['status']}
+    return JsonResponse(result)
